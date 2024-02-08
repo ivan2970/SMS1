8c9b8ec09802cf86be5338628d3edbea1544b607
Denis <op112233@ya.ru>
2024-02-08 12:17:26 +0300

    Добавлено текстовое поле ввода и проверка длины введенного текста.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def check_balance(login, password):
 
         if response.status_code == 200:
             response_data = response.json()
+            mb.showinfo("Баланс", f"Баланс счета {response_data["money"]}")
             return response_data["money"]
         else:
             mb.showerror("Ошибка!",f"Произошла ошибка проверки баланса {response.status_code}")
@@ -34,7 +35,11 @@ def send_sms():
     password='TCMS9L'
     sender='python2024'
     receiver = receiver_entry.get()
-    text = text_entry.get()
+    text = text_entry.get(1.0, END)
+
+    if len(text) > 160:
+        mb.showerror("Ошибка!", f"Длина вашего сообщения {len(text)}. Она не может  превышать 160 символов.")
+        return
 
     balance = check_balance(user, password)
     if balance:
@@ -43,7 +48,7 @@ def send_sms():
             if not validate_phone_number(receiver):
                 mb.showinfo("Ошибка!", "Некорректный номер телефона")
             else:
-                url = f"https://my3.webcom.mob/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
+                url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
                 try:
                     response = requests.get(url)
 
@@ -60,14 +65,14 @@ def send_sms():
 
 window = Tk()
 window.title("Отправка СМС")
-window.geometry("250x110")
+window.geometry("400x200")
 
-Label(text="Номер получателя: ").pack()
+Label(text="Номер получателя в формате 79*********: ").pack()
 receiver_entry = Entry()
 receiver_entry.pack()
 
 Label(text="Введите текст СМС").pack()
-text_entry = Entry()
+text_entry = Text(height=6, width=30)
 text_entry.pack()
 
 send_button = Button(text="Отправить СМС", command=send_sms)
